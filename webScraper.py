1aa0e178aaa346aae908c3b2da84d207c0181c30
Teeron Hajebi <teeron@Teerons-MacBook.attlocal.net>
2022-07-23 21:16:39 -0700

    Grabs needed dApp contracts

--- webScraper.py
+++ webScraper.py
@@ -2,33 +2,49 @@ import requests
 import json
 
 def main():
+	dAppList = listOfDApps()
+	#print(dAppList)
+	contractData(dAppList)
 
-#def 
-	# Grab data from dApp ranking page 
-	url = 'https://www.stateofthedapps.com/rankings/platform/ethereum'
+# Grab data from dApp ranking page 
+def listOfDApps():
 
-	page = requests.get(url)
-	with open('file.txt', 'w') as file:
-		file.write(page.text)
-
-	with open('file.txt') as file:
-	    htmlDoc = list(file)
-
+	# Grab data from dApp ranking page
 	dAppList = []
-	for item in htmlDoc:
-		if 'href="/dapps/' in item:
-			parsed = item.split()
-			for brokenDown in parsed:
-				if 'href="/dapps/' in brokenDown:
-					if 'submit/new' not in brokenDown and brokenDown not in dAppList:
-						end = brokenDown.index('\"', 13)
-						dAppList.append(brokenDown[13:end])
+ 
+	for page in range(1, 61):
+		if page == 1:
+			url = 'https://www.stateofthedapps.com/rankings/platform/ethereum'
+		else:
+			url = 'https://www.stateofthedapps.com/rankings/platform/ethereum?page=' + str(page)
+
+
+		page = requests.get(url)
+		with open('file.txt', 'w') as file:
+			file.write(page.text)
+
+		with open('file.txt') as file:
+		    htmlDoc = list(file)
+
+		for item in htmlDoc:
+			if 'href="/dapps/' in item:
+				parsed = item.split()
+				for brokenDown in parsed:
+					if 'href="/dapps/' in brokenDown:
+						if 'submit/new' not in brokenDown and brokenDown not in dAppList:
+							end = brokenDown.index('\"', 13)
+							dApp = brokenDown[13:end]
+							if portalDApps is True:
+								dAppList.append(dApp)
 
 	dAppList = list(set(dAppList))
+	return dAppList
 
-	# Iterate through names and modify urls to find specific dApp pages
-	# Create a JSON file to store partitioned data 
-	# (List comprised of dictionaries which contain dApp name, contract, category, description, and icon)
+
+# Iterate through names and modify urls to find specific dApp pages
+# Create a JSON file to store partitioned data 
+# (List comprised of dictionaries which contain dApp name, contract, category, description, and icon)
+def contractData(dAppList):
 
 	contractList = []
 	for dApp in dAppList:
@@ -70,7 +86,7 @@ def main():
 				dAppData['KovanContracts'] = kovanContracts
 
 				ETH = item[item.index('contractsMainnet') + 19:]
-				ETHContracts = ETH[:ETH.index('\"]')]	
+				ETHContracts = ETH[:ETH.index(']')]	
 				ETHContracts = ETHContracts.split('\",\"')
 				# print(ETHContracts)
 				dAppData['EthereumMainnetContracts'] = ETHContracts
@@ -100,27 +116,39 @@ def main():
 						contractData['dAppName'] = dAppName
 						contractData['networkName'] = key[:key.index('Contract')]
 
-						#if isValid(contractData) == True:
-						contractList.append(contractData)
+						if portalDApps(contractData) == True:
+							contractList.append(contractData)
 
 	with open('Data.json', 'w') as outfile:
 		json.dump(contractList, outfile)
 
+
+def portalDApps(dApp):
+
+	portalDApps = ['Opensea', 'Opensea TestNet', 'Rarible', 'Rarible Rinkeby', 'Aave', 'Aave V3', 'Aave V2', 'Aave V1', 
+				   'Decentraland', 'Uniswap V1', 'Uniswap V2', 'Uniswap V3', 'Uniswap', 'Compound', 'Sushi Swap', 'Known Origin', 
+				   'Lido', 'Superrare', 'CryptoVoxel', 'Gem', 'Bored Ape Yacht Club', 'Cryptopunk', 'Foundation', 'Zora', 
+				   'Sorare', 'Curv', 'CowSwap', 'ShibaSwap', 'InstadApp', 'DODO', 'Convex Finance', 'Liquity', 'Ribbon', 'Rabbithole', 
+				   'Layer3', 'Otherside', 'Hyy.pe', 'Sandbox', 'ENS', 'Zapper', 'DeBank', 'APY Vision', 'Rarity Tools']
+
+	if dApp in portalDApps:
+		return True
+	else:
+		return False
+
+
 # Function to verify is a contract is valid
 def isValid(contractData):
-	contractURL = 'https://etherscan.io/address/' + contractData['Contract Address']
+
+	contractURL = 'https://etherscan.io/address/' + contractData['contractAddress']
 
 	page = requests.get(contractURL)
-	with open('validation/' + contractData['Contract Address'] + '.txt', 'w') as file:
-		file.write(page.text)
-
 	print(page.text)
+	if contractData['dAppName'] in page.text:
+		return True
+	else:
+		return False
 
-	with open('validation/' + contractData['Contract Address'] + '.txt') as file:
-		if contractData['dApp'] in file:
-			return True
-		else:
-			return False
 
 if __name__ == '__main__':
 	main()
